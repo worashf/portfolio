cef5cfa3bea1d42bdf38b60f4ebf5192fbfe4492
worashf <uppert83@gmail.com>
2022-11-08 22:54:30 +0300

    Create nextjs app with tailwindcss configured

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return;
+
+  <div className="max-h-screen md:sticky md:top-0 z-50 text-white">
+    {/* Mobile sidebar */}
+    <div className=""></div>
+  </div>;
+};
+
+export default Navbar;
