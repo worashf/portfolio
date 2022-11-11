382766a9977be7b69b69da26056f566addef1346
worashf <uppert83@gmail.com>
2022-11-11 17:30:26 +0300

    Add skills and testimonial data

---
+++ data/skills.js
@@ -0,0 +1,80 @@
+import { SkipBack } from 'phosphor-react';
+
+const skills = [
+  {
+    title: 'HTML',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  },
+  {
+    title: 'CSS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+  },
+  {
+    title: 'Javascript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    title: 'Sass',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+  },
+  {
+    title: 'TailwindCSS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+  },
+  {
+    title: 'BootStrap',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+  },
+  {
+    title: 'React',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  },
+  {
+    title: 'NextJS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg',
+    style: { filter: 'invert(1)' },
+  },
+  {
+    title: 'Typescript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+  },
+  {
+    title: 'NodeJS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+  },
+  {
+    title: 'Git',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+  },
+
+  {
+    title: 'Figma',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+  },
+  {
+    title: 'Java',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
+  },
+  {
+    title: 'Spring Boot',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg',
+  },
+  {
+    title: 'Ruby',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg',
+  },
+  {
+    title: 'ROR',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg',
+  },
+  {
+    title: 'PostgreSQL',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+  },
+  {
+    title: 'MongoDb',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+  },
+];
+
+export default skills;
---
+++ data/testimonial.js
@@ -0,0 +1,21 @@
+const testimonials = [
+  {
+    quote:
+      'Worash highlights are hard work and caution.   For this, I have no doubts that he will take any project to the next level.',
+    name: 'Gabriel Fonseca',
+    job: 'Full-Stack Web Dev',
+  },
+  {
+    quote:
+      'A developer working for the group and  helping other developers in need. He is also genuinely a kind and encouraging person. ',
+    name: 'Elson Otake',
+    job: 'Full-Stack Web Dev',
+  },
+  {
+    quote:
+      'Quite skilled as a problem solver and  generous with his knowledge. The quality and speed of the work was excellent.',
+    name: 'Minilik Assefa',
+    job: 'Dupty manager at ICSMIS',
+  },
+];
+export default testimonials;
