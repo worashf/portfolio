14b9899a710ff74ecb607ad99fb66b5ac84fe096
worashf <uppert83@gmail.com>
2022-11-11 17:26:14 +0300

    Create testimonial components

---
+++ components/home/Testimonial.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import SectionTitle from '../layout/SectionTitle';
+import testimonials from '../../data/testimonial';
+
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col text-left  max-w-md md:max-w-full w-full  ">
+      <SectionTitle title="Why people love my work!" />
+      <div className="max-w-5xl m-auto md:max-h-[200px] grid grid-cols-1 gap-10 md:gap-10 md:grid-cols-3 items-start">
+        {testimonials.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="relative bg-fun-pink-darker border border-fun-pink-light p-5 rounded-lg h-full flex flex-col justify-between"
+            >
+              <p className="text-base italic relative testimonialQuote">
+                "{item.quote}"
+              </p>
+              <p className="mt-4 text-xs text-fun-gray">
+                <b className="text-fun-pink font-monospace">{item.name}</b> -{' '}
+                {item.job}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
