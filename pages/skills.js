6d3fab71ef88eddb751f3945662a0ee6ced9b8eb
worashf <uppert83@gmail.com>
2022-11-11 17:31:00 +0300

    Add skils page

---
+++ pages/skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PageContainer from '../components/layout/Container';
+import ToolSet from '../components/home/ToolSet';
+const skills = () => {
+  return (
+    <PageContainer
+      currentPage="Skills"
+      meta={{
+        desc: "I'm a passionate web developer and designer coding beautiful websites and apps.",
+      }}
+    >
+      <div className="pt-10  ">
+        <div className="md:mx-5 space-y-32  ">
+          <ToolSet />
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default skills;
