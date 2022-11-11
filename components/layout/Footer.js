edb18b9c30ecb8b4fbda0db66d9590962c55f974
worashf <uppert83@gmail.com>
2022-11-11 17:29:18 +0300

    Create footer component

---
+++ components/layout/Footer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Image from 'next/image';
+const Footer = () => {
+  return (
+    <footer className="flex flex-col  px-5 py-10 border-t border-fun-pink-darker z-5 bg-bg">
+      <div className="max-w-4xl w-full m-auto mt-8 pt-8 sm:mt-4 sm:pt-4 text-center text-fun-gray border-t border-fun-pink-dark">
+        <div className="flex flex-col items-center justify-center ">
+          <div className="inline-flex items-center uppercase text-xs font-bold tracking-widest">
+            Made with{' '}
+            <div className="space-x-2 inline-flex items-center -mt-1 ml-3">
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                  width="26"
+                  title="React"
+                />
+                <span className="sr-only">React</span>
+              </span>
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
+                  width="40"
+                  className="invert"
+                  title="NextJS"
+                />
+                <span className="sr-only">NextJS</span>
+              </span>
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+                  width="26"
+                  title="TailwindCSS"
+                />
+                <span className="sr-only">TailwindCSS</span>
+              </span>
+            </div>
+          </div>
+          <div className="mt-2 text-xs ">
+            Made by{' '}
+            <a
+              href="mailto:contact@braydentw.io"
+              className="text-fun-gray-light font-medium"
+            >
+              Worash Abocherugn
+            </a>
+            . All rights reserved.
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 text-center sm:text-right sm:-mt-12">
+        <a
+          className="w-auto inline-flex items-center sm:w-auto font-bold flex-shrink text-xs border border-fun-pink px-4 py-2 rounded-xl text-fun-pink cursor-pointer opacity-50"
+          href="https://github.com/worashf/portfolio"
+          target="_blank"
+          rel="nooreferrer"
+        >
+          <Image
+            src="/static/icons/github.svg"
+            width="16"
+            height="16"
+            alt="Github Icon"
+          />
+          <span className="ml-2">View Source Code </span>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
