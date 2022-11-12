e00d47d3a9c026185749d78954da7afc2a3c7467
worashf <uppert83@gmail.com>
2022-11-12 14:43:35 +0300

    Add portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ProjectList from '../components/projects/ProjectList';
+import PageContainer from '../components/layout/Container';
+import Heading from '../components/projects/Heading';
+import More from '../components/projects/More';
+const portfolio = () => {
+  return (
+    <PageContainer
+      currentPage="Projects"
+      meta={{
+        desc: "I'm a passionate web developer and designer coding beautiful websites and apps.",
+      }}
+    >
+      <div className="pt-2">
+        <div className="md:mx-5 space-y-10  ">
+          <Heading />
+          <ProjectList />
+          <More />
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default portfolio;
