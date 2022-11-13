eac89d2f5290e2e32c4902fc4a0b468d512b840d
worashf <uppert83@gmail.com>
2022-11-13 17:33:14 +0300

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PageContainer from '../components/layout/Container';
+import About from '../components/about/About';
+const AboutPage = () => {
+  return (
+    <PageContainer
+      currentPage="About"
+      meta={{
+        desc: "I'm a passionate web developer and designer coding beautiful websites and apps.",
+      }}
+    >
+      <div className="pt-10  ">
+        <div className="md:mx-5 space-y-20  ">
+          <About />
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default AboutPage;
