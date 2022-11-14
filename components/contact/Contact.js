2767bd714ba1ff0cbbd6621018031484e0f0aa7a
worashf <uppert83@gmail.com>
2022-11-14 15:27:48 +0300

    Style contact component

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -28,54 +28,77 @@ const Contact = () => {
             </h5>
           </a>
 
-          <ul class="space-y-1 text-lg  list-disc list-inside text-gray-300 ">
-            <li className="flex items-center">
-              <Envelope className="mr-20 text-fun-pink" size={40} />
-              <p className="text-xl text-blue-400">Uppert83@gmail.com</p>
+          <ul class="space-y-1 text-lg   list-disc list-inside text-gray-300 ">
+            <li className="flex flex-row w-full space-x-10 items-center  justify-start md:space-x-40 ">
+              <p className="text-3xl  w-1/4 ">Email:</p>
+              <div className="flex  flex-row items-center  hover:text-blue-800">
+                <Envelope
+                  className="mr-3 text-fun-pink hover:text-blue-400"
+                  size={30}
+                />
+                <p className="text-xl text-blue-400 hover:text-fun-pink">
+                  Uppert83@gmail.com
+                </p>
+              </div>
             </li>
-            <li className="flex items-center">
-              <LinkedinLogo className="mr-20 text-fun-pink" size={40} />
-              <Link
-                href="     https://www.linkedin.com/in/worash-abocherugn/"
-                target="_blank"
-                rel="nooreferrer"
-                className="text-2xl text-blue-400 hover:text-fun-pink"
-              >
-                worash-abocherugn
-              </Link>
+            <li className="flex flex-row w-full space-x-10 items-center  justify-start md:space-x-40 ">
+              <p className="text-3xl w-1/4 ">LinkedIn:</p>
+              <div className="flex  flex-none flex-row items-center  hover:text-blue-800">
+                <LinkedinLogo
+                  className=" mr-3 text-fun-pink hover:text-blue-400"
+                  size={30}
+                />
+                <Link
+                  href="     https://www.linkedin.com/in/worash-abocherugn/"
+                  target="_blank"
+                  rel="nooreferrer"
+                  className="text-2xl text-blue-400 hover:text-fun-pink"
+                >
+                  worash-abocherugn
+                </Link>
+              </div>
             </li>
-            <li className="flex items-center">
-              <GithubLogo className="mr-20 text-fun-pink" size={40} />
-              <Link
-                href="https://github.com/worashf/"
-                target="_blank"
-                rel="nooreferrer"
-                className="text-2xl text-blue-400 hover:text-fun-pink"
-              >
-                worashf
-              </Link>
+            <li className="flex flex-row  w-full space-x-10 items-center md:space-x-40">
+              <p className="text-3xl w-1/4">Github:</p>
+              <div className="flex  flex-row items-center  hover:text-blue-800">
+                <GithubLogo className="mr-3 text-fun-pink" size={30} />
+                <Link
+                  href="https://github.com/worashf/"
+                  target="_blank"
+                  rel="nooreferrer"
+                  className="text-2xl text-blue-400 hover:text-fun-pink"
+                >
+                  worashf
+                </Link>
+              </div>
             </li>
-            <li className="flex items-center">
-              <TwitterLogo className="mr-20 text-fun-pink" size={40} />
-              <Link
-                href="https://github.com/worashf/"
-                target="_blank"
-                rel="nooreferrer"
-                className="text-2xl text-blue-400 hover:text-fun-pink"
-              >
-                @worash-aboche
-              </Link>
+            <li className="flex w-full flex-row space-x-10 items-center md:space-x-40">
+              <p className="text-3xl w-1/4">Tiwtter:</p>
+              <div className="flex  flex-row items-center  hover:text-blue-800">
+                <TwitterLogo className="mr-3 text-fun-pink" size={30} />
+                <Link
+                  href="https://twitter.com/WorashAboche"
+                  target="_blank"
+                  rel="nooreferrer"
+                  className="text-2xl text-blue-400 hover:text-fun-pink"
+                >
+                  @WorashAboche
+                </Link>
+              </div>
             </li>
-            <li className="flex items-center">
-              <FacebookLogo className="mr-20 text-fun-pink" size={40} />
-              <Link
-                href="https://github.com/worashf/"
-                target="_blank"
-                rel="nooreferrer"
-                className="text-2xl text-blue-400 hover:text-fun-pink"
-              >
-                Worash abocherugn
-              </Link>
+            <li className="flex flex-row space-x-10  w-full items-center md:space-x-40">
+              <p className="text-3xl w-1/4">Facebook:</p>
+              <div className="flex  flex-row items-center  hover:text-blue-800">
+                <FacebookLogo className="mr-5 text-fun-pink" size={30} />
+                <Link
+                  href="https://www.facebook.com/profile.php?id=100006018075352"
+                  target="_blank"
+                  rel="nooreferrer"
+                  className="text-2xl text-blue-400 hover:text-fun-pink"
+                >
+                  Worash abocherugn
+                </Link>
+              </div>
             </li>
           </ul>
         </div>
