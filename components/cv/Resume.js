446a5bf2ae5284b2417faf792c62ea1f92da9a10
worashf <uppert83@gmail.com>
2022-11-15 14:55:07 +0300

    Add cv component

---
+++ components/cv/Resume.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { Document } from 'react-pdf';
+// import { Page } from 'react-pdf';
+// import ResumePdf from '../../resume/worash-resume.pdf';
+// const Resume = () => {
+//   return (
+//     <>
+//       <Document file={ResumePdf}>
+//         <Page pageNumber={1} />
+//       </Document>
+//     </>
+//   );
+// };
+
+export default Resume;
