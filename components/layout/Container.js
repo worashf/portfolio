1fad52da7b197feb101bbb4a039d8bdf0f0a99a5
worashf <uppert83@gmail.com>
2024-03-26 08:55:40 +0300

    Update issues

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -11,7 +11,7 @@ const PageContainer = ({ currentPage, pageDesc, meta: { desc },link:{link} ,chil
   return (
     <div
       className="w-full flex flex-col md:flex-row   min-h-screen opening-box-animate-paddin text-white  "
-      style={{ maxWidth: '1200px' }}
+      // style={{ maxWidth: '1200px' }}
     >
       <Head>
         <title>{pageTitle}</title>
@@ -35,7 +35,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
       ></noscript> */}
 
       <Navbar />
-      <div className="p-3 w-full ">
+      <div className=" w-full ">
         {children}
         <Footer />
       </div>
